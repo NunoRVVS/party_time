b5452fcdd13df6c2f46d8eb601c7b70b9c5e260e
Jose Canario <jea.canario@gmail.com>
2024-11-18 17:09:32 +0000

    db seeding working

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+PartyItem.destroy_all
+User.destroy_all
+
+
+3.times do |t|
+  user = User.create!(name: Faker::Name.name, email:"#{t}anemail@provider.com", password:"123456")
+  p user
+end
+10.times do
+  item= PartyItem.create!( price: rand(30..150), name: Faker::Ancient.god, description: Faker::Books::Dune.quote, user: User.all.sample())
+  p item
+end
