f216b31799f3efbc15f7162ab1ae5dd3edb79999
Jose Canario <jea.canario@gmail.com>
2024-11-19 12:12:03 +0000

    added order creation functionality + rudimentary  index & show views

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,21 +4,35 @@ class OrdersController < ApplicationController
     @orders = current_user.orders
   end
 
+  def new
+    @party_item = PartyItem.find(params[:party_item_id])
+    @user = current_user
+    @order = Order.new
+  end
+
+  def show
+    @order = Order.find(params[:id])
+    @party_item = @order.party_item
+    @user = @order.user
+  end
+
   def create
-    @partyitem = Party_item.find(params[:party_item_id])
-    @order = @party_item.orders.new(order_params)
+    @party_item = PartyItem.find(params[:party_item_id])
+    @order = Order.new(order_params)
     @order.user = current_user
+    @order.party_item = @party_item
+    @order.amount = (@order.end - @order.start).to_i * @party_item.price
 
     if @order.save
-      redirect_to @order, notice: 'Ok.'
+      redirect_to order_path(@order), notice: 'Order Successfully Placed'
     else
-      render :new, alert: 'Wrong.'
+      render :new, alert: 'Error in placing your order'
     end
   end
 
   private
 
   def order_params
-    params.require(:order).permit(:start, :end, :amount, :user_id, :party_item_id)
+    params.require(:order).permit(:start, :end, :amount)
   end
 end
