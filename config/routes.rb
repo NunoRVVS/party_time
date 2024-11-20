2e940b8e35cbd8a1eec2f382554ddf39e9d1f965
Alexandru Posirca <alexposh@gmail.com>
2024-11-20 10:39:25 +0000

    patch on update added

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :party_items, only: [:index, :show, :create, :new, :edit] do
+  resources :party_items, only: [:index, :show, :create, :new, :edit, :update] do
     resources :orders, only: [:new, :create]
   end
 
